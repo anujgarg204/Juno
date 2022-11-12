f57661c77e42e865dfbf347bc3c2e9226d90eb41
Anuj Garg <anuj@Anujs-MacBook-Air.local>
2022-11-12 17:40:25 +0530

    Ursa first git change

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World! Nice to meet you");
+        System.out.println("Ursa batch 1st branch change");
     }
 }
