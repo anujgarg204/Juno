41af876a6fa95af1bb6b4ca1391e02f637024688
Anuj Garg <anuj@Anujs-MacBook-Air.local>
2022-11-12 17:41:17 +0530

    Ursa batch second commit

--- src/App.java
+++ src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World! Nice to meet you");
         System.out.println("Ursa batch 1st branch change");
+        System.out.println("Ursa batch 2nd branch change");
     }
 }
